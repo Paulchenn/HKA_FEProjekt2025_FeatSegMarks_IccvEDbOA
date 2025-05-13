b82fa8917b5366797f6a59e7703cd1152fe0f8c4
Paulchenn <154928432+Paulchenn@users.noreply.github.com>
2025-05-13 22:31:21 +0200

    Continued in Training part <-- not finished yet (e. g. validation part still missing)

--- train_SDbOA_imageNet.py
+++ train_SDbOA_imageNet.py
@@ -2,12 +2,13 @@ import json
 import os
 
 import torch
-from torch import optim
+from torch import nn, optim
 
 from torchvision import transforms
 from utils.get_ImageNet import main as get_ImageNet
 from models import generation
 from models.resnet import ResNet18, BasicBlock
+from utils.modules  import *
 
 # Path of the current script
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -33,6 +34,11 @@ LR_CLS = config["learning_rate"]["classifier"]
 if __name__ == "__main__":
     image_size = 128
 
+    # === Initialize Trainiingclasses ===
+    emse    = EMSE()    # Initialize EMSE class
+    tsd     = TSD()     # Initialize TSD class
+    tsg     = TSG()     # Initialize TSG class
+
     # Transformations for preprocessing training data
     transform_train = transforms.Compose([
         transforms.Resize(image_size),
@@ -55,11 +61,7 @@ if __name__ == "__main__":
     
     # Set best accuracy to 0 to be sure that first accuracy is better
     best_acc = 0
-
-    # Resize-Operations
-    re12 = transforms.Resize((12, 12))  # scales to 12x12
-    re32 = transforms.Resize((32, 32))  # scales to 32x32
-
+    
     # === Initialize networks (and move to CPU/GPU) ===
     netG    = generation.generator(GEN_IN_DIM).to(DEVICE)       # Generator network with input size GEN_IN_DIM
     netD    = generation.Discriminator().to(DEVICE)             # Discriminator to distinguish real/fake images
@@ -70,4 +72,85 @@ if __name__ == "__main__":
     optimD = optim.Adam(netD.parameters(), lr=LR_DISC, betas=(0., 0.99))    # Optimizer for Discriminator (GAN-typical Betas)
     optimC = optim.Adam(cls.parameters(), lr=LR_CLS, betas=(0., 0.99))      # Optimizer for Classifier (GAN-typical Betas)
 
-    # === Initialize loss functions ===
+    # === Initialize loss functions === 
+    L1_loss = nn.L1Loss()                # Absoulte Error (e.g. for Reconstruction)
+    CE_loss = nn.CrossEntropyLoss()      # Classification Loss (for multi-class outputs like CIFAR-10 Classes)
+
+    ##### ===== TRAINING ===== #####
+    # Initailisation of values for early stop
+    no_improvement_counter = 0
+
+    for epoch in range(EPOCHS):
+        print(epoch)
+
+        # === Training Phase ===
+        netG.train()
+        netD.train()
+        cls.train()
+
+        # Reduce learning rate after 60 epochs
+        if epoch == 60:
+            for param_group in optimG.param_groups:
+                param_group['lr'] = LR_GEN / 10
+            for param_group in optimD.param_groups:
+                param_group['lr'] = LR_DISC / 10
+            for param_group in optimC.param_groups:
+                param_group['lr'] = LR_CLS / 10
+
+        for i, (img, label) in enumerate(train_dataset):
+            img     = img.to(DEVICE)
+            label   = label.to(DEVICE)
+
+            # === EMSE >>>
+            edgeMap = emse.doEMSE(img)
+            # <<< EMSE ===
+
+            # === TSD >>>
+            deformedImg = tsd.doTSD(img)
+            # <<< TSD ===
+
+            # === TSG >>>
+            netD, netG, cls, optimD, optimG, optimC, CE_loss, L1_loss, loss_tot = tsg.doTSG(
+                img,
+                label,
+                edgeMap,
+                deformedImg,
+                netD,
+                netG,
+                cls,
+                optimD,
+                optimG,
+                optimC,
+                CE_loss,
+                L1_loss,
+            )
+            # <<< TSG ===
+
+            # Print loss values after every 5 iterations
+            if i % 5 == 0:
+                print('Epoch[', epoch + 1, '/', EPOCHS, '][', i + 1, '/', len(train_dataset), ']: TOTAL_LOSS', loss_tot.item())
+
+            # For debugging: Stop after a certain number of iterations
+            if DEBUG_MODE and i >= DEBUG_ITERS_START + DEBUG_ITERS_AMOUNT:
+                break
+
+        # Save the results and models afer every epochs
+        path2save = './Result/cifar_gan/visualization'
+        fixed_p = path2save + '/' + str(epoch) + '.png'
+        if not os.path.exists(path2save):
+            os.makedirs(path2save)
+        try:
+            show_result(epoch, path=fixed_p, netG=netG)  # Shows the result of actual epoch
+        except IndexError as e:
+            print(f"IndexError: {e}")
+        torch.save(netG.state_dict(), './Result/cifar_gan/tuned_G_' + str(epoch) + '.pth')  # Saves Generator
+        torch.save(netD.state_dict(), './Result/cifar_gan/tuned_D_' + str(epoch) + '.pth')  # Saves Diskriminator
+
+        # === Validation Phase ===
+        netG.eval()
+        netD.eval()
+        cls.eval()
+
+        # Counter for correct predictions and total predictions
+        correct = torch.zeros(1).squeeze().to(device)
+        total   = torch.zeros(1).squeeze().to(device)
