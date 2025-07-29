2d695e0b8697de922b149fd04e9696b0958a854e
Kuka PC <glpa1013@h-ka.de>
2025-07-29 14:39:14 +0200

    Added debug prints

--- utils/modules.py
+++ utils/modules.py
@@ -381,9 +381,6 @@ class TSG:
         L1_loss,
         downSize=12
     ):
-        print("G_rough contiguous after generateImg:", e_extend.is_contiguous())
-        print("G_rough contiguous after generateImg:", e_deformed.is_contiguous())
-
         # Initialize loss variables
         loss = SimpleNamespace()
 
@@ -395,6 +392,8 @@ class TSG:
 
         # === Step 1: Phase 1 – Rough Generation (Eextend + Itxt) ===
         # Generate rough image (Stage 1)
+        print("G_rough contiguous after e_extend:", e_extend.is_contiguous())
+        print("G_rough contiguous after img_blur:", img_blur.is_contiguous())
         G_rough = self.generateImg(mn_batch, netG, e_extend, img_blur)  # Input: edge + blurred image
         G_rough = G_rough.contiguous()
 
